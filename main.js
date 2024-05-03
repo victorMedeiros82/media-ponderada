84fe784a0246160d051ce877ac08cd09c564d9f1
Victor Medeiros <victor.medeiros82@gmail.com>
2024-05-03 15:30:02 -0300

    Movendo os arquivos js que estavam no html para o arquivo main js

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 let button = document.querySelector("#botao_verificar");
 
 button.addEventListener("click", verificar);
-
 function verificar() {
-  /* TODO.4: transfira a solução desenvolvida no arquivo html para cá */
+  let n1 = document.querySelector('#n1').value
+  let n2 = document.querySelector('#n2').value
+  let n3 = document.querySelector('#n3').value
+  let n4 = document.querySelector('#n4').value
+  let p1 = document.querySelector('#p1').value
+  let p2 = document.querySelector('#p2').value
+  let p3 = document.querySelector('#p3').value
+  let p4 = document.querySelector('#p4').value
+  let presenca = document.querySelector('#presenca').value
 
-
-const button = document.querySelector("#botao_verificar");
-button.addEventListener("click", verificar);    
+  let media = calculaMedia(n1, n2, n3, n4, p1 = 1, p2 = 1, p3 = 1, p4 = 1)
+  let resultado = verificaAprovacao(media, presenca = 0.75);       
+  
+  document.querySelector("#media").textContent = media;
+  document.querySelector("#resultado").textContent = resultado;   
 }
+console.log(verificar())
