f68d7a1f771131a01143947f535165b7fd86d8f1
Victor Medeiros <victor.medeiros82@gmail.com>
2024-05-03 16:48:17 -0300

    Mundando o tipo dos imputs das notas de text para number e colocando range de min e max de 0 a 10

--- scripts/media.js
+++ scripts/media.js
@@ -3,12 +3,7 @@ function calculaMedia(n1, n2, n3, n4, p1 = 1, p2 = 1, p3 = 1, p4 = 1) {
 }
 
 function verificaAprovacao(media, presenca = 0.75) {
-//    if (media>=7&&presenca>=0.75) {
-//         return "Aprovado"
-//    } else {
-//         return "Reprovado"
-//    }
-    return media >= 7 && presenca >= 0.75
+    return media >= 7 && presenca >= 0.75 ? "Aprovado" : "Reprovado"
 }
 
 function verificaSituacao(n1, n2, n3, n4, presenca = 0.75, p1 = 1, p2 = 1, p3 = 1, p4 = 1) {
