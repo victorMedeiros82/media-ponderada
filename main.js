2d60a142f8fb8da897cea61277730425371b0a66
Victor Medeiros <victor.medeiros82@gmail.com>
2024-05-08 15:59:14 -0300

    alteraçoes

--- main.js
+++ main.js
@@ -2,17 +2,21 @@ let button = document.querySelector("#botao_verificar");
 
 button.addEventListener("click", verificar);
 function verificar() {
-  let n1 = document.querySelector('#n1').value
-  let n2 = document.querySelector('#n2').value
-  let n3 = document.querySelector('#n3').value
-  let n4 = document.querySelector('#n4').value 
-  let p1 = document.querySelector('#p1').value
-  let p2 = document.querySelector('#p2').value
-  let p3 = document.querySelector('#p3').value
-  let p4 = document.querySelector('#p4').value
+  let n1 = parseFloat(document.querySelector('#n1').value)
+  let n2 = parseFloat(document.querySelector('#n2').value)
+  let n3 = parseFloat(document.querySelector('#n3').value)
+  let n4 = parseFloat(document.querySelector('#n4').value) 
+  let p1 = parseFloat(document.querySelector('#p1').value)
+  let p2 = parseFloat(document.querySelector('#p2').value)
+  let p3 = parseFloat(document.querySelector('#p3').value)
+  let p4 = parseFloat(document.querySelector('#p4').value)
   let presenca = document.querySelector('#presenca').value
   
-  let media = calculaMedia(n1, n2, n3, n4, p1 , p2 , p3 , p4 ).toFixed(2)
+  // let media = calculaMedia(n1, n2, n3, n4, p1 , p2 , p3 , p4 ).toFixed(2)
+  // if (media==NaN){
+  //   document.querySelector("#media").textContent = "TESTE"
+  // }
+  // console.log(media)
   let resultado = verificaAprovacao(media, presenca);       
   
   let resultadoMedia = document.querySelector("#media");
--- scripts/media.js
+++ scripts/media.js
@@ -1,4 +1,5 @@
 function calculaMedia(n1, n2, n3, n4, p1 = 1, p2 = 1, p3 = 1, p4 = 1) {
+    console.log(typeof n1, n2, n3, n4, p1 , p2 , p3 , p4)
     return (n1 * p1 + n2 * p2 + n3 * p3 + n4 * p4) / (p1 * 1 + p2 * 1 + p3 * 1 + p4 * 1);
 }
 
@@ -14,6 +15,5 @@ function verificaAprovacao(media, presenca = 0.75) {
 function verificaSituacao(n1, n2, n3, n4, presenca = 0.75, p1 = 1, p2 = 1, p3 = 1, p4 = 1) {
     let media = calculaMedia(n1, n2, n3, n4, p1 = 1, p2 = 1, p3 = 1, p4 = 1);
     let aprovado = verificaAprovacao(media, presenca)
-
     return { media, aprovado }
 }
